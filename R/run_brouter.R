f79f5ae3ed92d8b74cf5317d98f28ae815fe2a80
Adam Watkins <adam@stupidpupil.co.uk>
2022-06-12 00:00:11 +0100

    Add run_brouter()

---
+++ R/run_brouter.R
@@ -0,0 +1,20 @@
+run_brouter <- function(code, brouter_dir = dir_working("brouter"), port=17777L) {
+
+  brouter_jar <- Sys.glob(paste0(brouter_dir, "/brouter-*-all.jar"))
+
+  px <- start_program(
+      "java", c(
+        java_args(), 
+        "-cp", brouter_jar,
+        "btools.server.RouteServer",
+        dir_output("brouter"),
+        paste0(brouter_dir, "/profiles2/"),
+        ".", # Custom profiles
+        port,
+        1L, # Threads
+        "0.0.0.0"),
+      "BRouter.+", timeout=20)
+
+
+  return(px)
+}
