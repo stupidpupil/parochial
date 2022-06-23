334efc5e8c1ae4d7df2bff7d87b198ca200774d0
Adam Watkins <adam@stupidpupil.co.uk>
2022-06-23 23:22:38 +0100

    Cope with missing MD5 for Terrain 50

--- R/cache_key_for_terrain50_url.R
+++ R/cache_key_for_terrain50_url.R
@@ -15,7 +15,27 @@ cache_key_for_terrain_50_url <- function(terrain_50_url) {
   terrain_50_directory <- httr::GET(base_url) %>%
     httr::content(type='text')  %>% jsonlite::fromJSON()
 
-  terrain_50_directory %>%
-    filter(url == terrain_50_url) %>%
-    pull(md5) %>% first()
+  terrain_50_entry <- terrain_50_directory %>%
+    filter(url == terrain_50_url)
+
+  stopifnot(nrow(terrain_50_entry) == 1)
+
+  ret <- terrain_50_entry %>% pull(md5) %>% first()
+
+  if(is.na(ret)){
+    ret <- terrain_50_entry %>% pull(size) %>% first()
+  }
+
+  if(is.na(ret)){
+    # Terrain 50 updates *around* July
+    # TODO: Use this fact to produce a more sensible cache key
+    ret <- lubridate::today() %>% add strftime("%Y-%m") 
+  }
+
+  ret <- openssl::sha1(paste0(
+    ret,
+    terrain_50_url
+  )) %>% as.character()
+
+  return(ret)
 }
