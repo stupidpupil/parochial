b8c84f2d99ab003401287bcf77e0a875120d2a3b
Adam Watkins <adam@stupidpupil.co.uk>
2022-06-30 11:19:33 +0100

    Add port check to run_brouter()

--- R/run_brouter.R
+++ R/run_brouter.R
@@ -1,5 +1,7 @@
 run_brouter <- function(code, brouter_dir = dir_working("brouter"), port=17777L) {
 
+  checkmate::assert_integer(port, lower = 1024L, upper=49151L, len=1L)   # TODO - find an open port
+
   brouter_jar <- Sys.glob(paste0(brouter_dir, "/brouter-*-all.jar"))
 
   px <- start_program(
